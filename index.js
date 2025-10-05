7f609c5ad17a523963f423dc531efea06dc73345
Haak8267 <haroldaannancy@gmail.com>
2025-10-05 15:03:04 +0000

    week 2

--- index.js
+++ index.js
@@ -1,35 +1,78 @@
 const express = require("express")
+const path = require("path")
 
 //Intialization
 const server = express()
 
-//controller
+// Body Parser Middleware (converts the data)
+server.use(express.urlencoded({extended:true}))
+server.use(express.json());
+
+//Middleware (the system checkpoint)
+const logger = (req,res,next)=>{
+    console.log("this middleware is working perfectly")
+    next();
+}
+
+const logger1 = (req,res,next)=>{
+    console.log("This is the product middleware and it's working perfectly")
+    next();
+}
+
+//Serving Files
+const serving = (req,res)=>{
+    //Finding the file
+    const homepath = path.join(__dirname, "public", "index.html")
+    //Serving the file
+    res.sendFile(homepath)
+}
+
+const delivering = (req,res)=>{
+    //Finding the file
+    const productPath = path.join(__dirname, "public", "product.html")
+    //Serving the file
+    res.sendFile(productPath)
+}
+
+//Serving all the pages
+server.use(express.static('public'))
+
+
+//Controller
 const Homepage = (req,res)=>{
-    res.send("This is the home page")
+    res.send("This is the home Page")
 }
 
 const Products = (req,res)=>{
-    res.send("This is my Product page")
+    res.send("This is my Product Page")
 }
 
-const Gallery = (req,res)=>
+const Gallery = (req,res)=>{
     res.send("Pictures from the Gallery")
+}
 
 const About = (req,res)=>{
     res.send("This is my About Page")
 }
 
 const Contact = (req,res) =>{
-    res.send("This is my Contact page")
+    res.send("This is my Contact Page")
+}
+
+const loginPage =(req,res)=>{
+    console.log(req.body)
+    res.send('Thank you')
 }
 
 
 //routes
-server.use('/Products',Products)
+// server.use(logger)
+server.use('/Products',logger1, Products)
+server.use('/login',loginPage)
 server.use('/Gallery', Gallery)
 server.use('/About', About)
-server.use('/Contact', Contact)
-server.use('/',Homepage)
+server.use('/Contact', delivering)
+server.use('/',serving)
 
 
 // starting server
